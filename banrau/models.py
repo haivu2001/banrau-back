c7c9eb45494532eaa3ece00f50bfb1da2dae6503
hvhvdevdev <86674264+hvhvdevdev@users.noreply.github.com>
2021-11-27 16:59:08 +0700

    category return name

--- banrau/models.py
+++ banrau/models.py
@@ -5,7 +5,10 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=128, null=False)
-    
+
+    def __str__(self):
+        return "Danh mục " + self.name
+
 
 class Product(models.Model):
     name = models.CharField(max_length=128, null=False)
